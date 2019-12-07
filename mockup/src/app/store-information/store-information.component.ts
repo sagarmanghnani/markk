c087fc2d9f4e4907c00362252155ff38427cc915
sagarmanghnani <sagarmanghnani@gmail.com>
2019-12-07 23:18:38 +0530

    added google maps

--- mockup/src/app/store-information/store-information.component.ts
+++ mockup/src/app/store-information/store-information.component.ts
@@ -1,17 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Sanitizer } from '@angular/core';
 import { StoreDetails } from 'src/modals/store-details';
 import { ReviewService } from 'src/services/review.service';
+import { DomSanitizer } from '@angular/platform-browser';
 
+
+
+declare var google;
 @Component({
   selector: 'app-store-information',
   templateUrl: './store-information.component.html',
   styleUrls: ['./store-information.component.scss'],
 })
 export class StoreInformationComponent implements OnInit {
-
   storeDetail:StoreDetails = new StoreDetails;
   constructor(
-    public reviewService:ReviewService
+    public reviewService:ReviewService,
+    public sanitizer:DomSanitizer
   ) { }
 
   ngOnInit() {
@@ -29,6 +33,10 @@ export class StoreInformationComponent implements OnInit {
 
   storeRating(){
     return `../../assets/${this.storeDetail.store_rating_icon}.png`;
+  }
+
+  embedGoogleMap(){
+    return this.sanitizer.bypassSecurityTrustResourceUrl(`https://www.google.com/maps/embed/v1/place?key=AIzaSyCVA_4jw6LE8aRbeIbHjZV0ZObC-s3ag-k&q=pali`);
   }
 
 
